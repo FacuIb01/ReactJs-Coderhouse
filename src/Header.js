639a8df4ec7779610f93cd9570335497a4585886
facu.ib <facundoemiliano.21@gmail.com>
2021-12-13 19:46:58 -0300

    creacion del menu

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+function Header (){
+    return (<header>
+        <a href="/" className="logo">MamahuevoGymStore</a>
+        <nav>
+            <ul className="navUl">
+                <a href="/"><li className="home"><ion-icon name="home-outline"size="large"></ion-icon></li></a>
+                <a href="/"><li className="tienda"><ion-icon name="barbell-outline" size="large"></ion-icon></li></a>
+                <a href="/"><li className="carrito"><ion-icon name="cart-outline"size="large"></ion-icon></li></a>
+            </ul>
+        </nav>
+    </header>)
+}
+
+export default Header
